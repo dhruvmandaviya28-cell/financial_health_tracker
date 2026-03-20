1a52abf3f6bd83f1c4c8ce638ab533bb086facd9
Dhruv Mandaviya <dhruvmandaviya23@gmail.com>
2026-03-20 13:03:03 +0530

    Delete finsight-react (3)/finsight-react directory

--- finsight-react (3)/finsight-react/finsight-react/src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
-
-/** Convenience hook to access AuthContext values */
-export function useAuth() {
-  const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error('useAuth must be used inside <AuthProvider>');
-  return ctx;
-}
